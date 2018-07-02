91e6f9123169122ffca6cd24b7d552eb41d50013
John Levermore <john.levermore@zhibek.com>
2018-07-02 12:23:54 +0100

    Remove ZendSkeletonApplication specific text / logic.

--- config/autoload/zend-developer-tools.docker.php
+++ config/autoload/zend-developer-tools.docker.php
@@ -1,14 +1,6 @@
 <?php
 /**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-/**
  * This is configuration for the ZendDeveloperTools development toolbar.
- *
- * It will be enabled when you enable development mode.
  */
 return [
     'zenddevelopertools' => [
